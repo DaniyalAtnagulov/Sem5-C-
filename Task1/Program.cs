3dfef75fff1a612772a13b674ff7a8a6c81ad777
Daniyal~ <danialastrachan@gmail.comgit config --global user.email danialastrachan@gmail.com>
2022-11-03 23:39:42 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int [] GetArray (int size, int minValue, int maxValue){
+    int[] res=new int[size];
+    for (int i=0;i<size;i++){
+        res[i]=new Random().Next(minValue, maxValue);
+        //Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array=GetArray(4,100,999);
+int even=0;
+foreach(int el in array)
+    if (el%2==0)
+    even+=1;
+Console.WriteLine($"the number of even numbers is {even} ");
