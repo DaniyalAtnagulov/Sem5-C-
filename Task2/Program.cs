cbb74cb2dfd600a62c1c68ea80b991aa175d3c24
Daniyal~ <danialastrachan@gmail.comgit config --global user.email danialastrachan@gmail.com>
2022-11-04 03:21:59 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res=new int[size];
+    for (int i=0;i<size;i++){
+        res[i]=new Random().Next(minValue, maxValue+1);
+            // Console.Write($"{res[i]} ");
+            
+    }
+    return res;
+}
+int CountOddElements (int[] arr){
+    int sum=0;
+    for (int z=1; z< arr.Length;z+=2)
+    sum+=arr[z];
+    return sum;
+    
+}
+int[] array=GetArray(4,-100, 100); //В данном случае размер массива и ограничение размера чисел в нем ввел, ориентируясь на пример в задаче.
+Console.WriteLine();
+Console.Write ($"The sum of odd elements is {CountOddElements (array)}");
